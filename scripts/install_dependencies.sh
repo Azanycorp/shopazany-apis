6d0bddfffd1ae9ea8328ab5a8206cbc3733d82d5
Azanycorp <128836047+Azanycorp@users.noreply.github.com>
2024-07-03 18:49:59 +0100

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# uncomment for new installations
+#sudo amazon-linux-extras enable php8.3
+#sudo yum clean metadata
+#sudo yum install php php-fpm php-mysqlnd -y
+#sudo systemctl start php-fpm
+#sudo systemctl enable php-fpm
+
+# Install Composer
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#php composer-setup.php --install-dir=/usr/local/bin --filename=composer
