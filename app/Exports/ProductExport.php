c368d63dd999ed094e65788a7fbd3f42360d55c9
onyiimesi <onyedikachukwu62@gmail.com>
2024-07-31 19:44:01 +0100

    Export fix

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -7,11 +7,18 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 
 class ProductExport implements FromCollection
 {
+    protected $userId;
+
+    public function __construct($userId)
+    {
+        $this->userId = $userId;
+    }
+
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Product::all();
+        return Product::where('user_id', $this->userId)->get();
     }
 }
