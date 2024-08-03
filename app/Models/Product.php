1bb14113a5bf3508d190dba99e6bf2d8853797fa
onyiimesi <onyedikachukwu62@gmail.com>
2024-08-03 08:56:30 +0100

    Added country to product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -28,16 +30,22 @@ class Product extends Model
         'minimum_order_quantity',
         'image',
         'added_by',
+        'country_id',
         'status'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function productimages()
+    public function productimages(): HasMany
     {
         return $this->hasMany(ProductImage::class, 'product_id');
+    }
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country_id');
     }
 }
