5972fbc7f68a99b99f0215673fed86512a34e6c5
emmawhocoded <emmawhocoded@gmail.com>
2025-01-05 15:03:30 +0100

    Code refactoring

---
+++ app/Http/Controllers/Api/B2B/Buyer/ProfileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\B2B\Buyer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    //
+}
