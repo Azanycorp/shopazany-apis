f5aed647e1b89f5282bfb3a6bc66c2a7d036b593
emmawhocoded <emmawhocoded@gmail.com>
2025-01-10 22:50:46 +0100

    Started implementing Seller Withdrawal method

--- routes/b2b.php
+++ routes/b2b.php
@@ -13,19 +13,22 @@ use App\Http\Controllers\Api\B2B\B2BAccountController;
 
 // B2B
 Route::middleware(['throttle:apis'])->group(function () {
-    Route::prefix('b2b/connect')->controller(B2BAccountController::class)->group(function () {
-        Route::post('/login', 'login');
-        Route::post('/login/verify', 'loginVerify');
-        Route::post('/seller/signup', 'signup');
-        Route::post('/forgot/password', 'forgot');
-        Route::post('/reset/password', 'reset');
-        Route::post('/signup/resend', 'resendCode');
-        Route::post('/logout', 'logout');
-        Route::post('/verify', 'verify');
+    Route::prefix('b2b/connect')
+        ->controller(B2BAccountController::class)
+        ->group(function () {
 
-        // Buyer Onboarding
-        Route::post('/buyer/signup', 'buyerOnboarding');
-    });
+            Route::post('/login', 'login');
+            Route::post('/login/verify', 'loginVerify');
+            Route::post('/seller/signup', 'signup');
+            Route::post('/forgot/password', 'forgot');
+            Route::post('/reset/password', 'reset');
+            Route::post('/signup/resend', 'resendCode');
+            Route::post('/logout', 'logout');
+            Route::post('/verify', 'verify');
+
+            // Buyer Onboarding
+            Route::post('/buyer/signup', 'buyerOnboarding');
+        });
 
     Route::prefix('b2b')->controller(B2BController::class)->group(function () {
         Route::post('/business/information', 'businessInformation');
@@ -55,11 +58,12 @@ Route::group(['middleware' => ['auth:api'], 'prefix' => 'b2b'], function () {
                 Route::post('/order-feeback', 'orderFeeback');
             });
             //payment method
-            Route::prefix('payment-method')->group(function () {
-                Route::get('/', 'allMethods');
-                Route::get('/details/{id}', 'viewPaymentMethodDetails');
-                Route::post('/update/{id}', 'updatePaymentMethod');
-                Route::delete('/details/{id}', 'deletePaymentMethod');
+            Route::prefix('withdrawal-method')->group(function () {
+                Route::get('/', 'allWithdrawalMethods');
+                Route::post('/add', 'addWithdrawalMethod');
+                Route::get('/details/{id}', 'getWithdrawalMethod');
+                Route::post('/update/{id}', 'updateWithdrawalMethod');
+                Route::delete('/delete/{id}', 'deleteWithdrawalMethod');
             });
             //complaints log
             Route::get('/refund/request', 'getComplaints');
