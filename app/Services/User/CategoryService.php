c5109dce19737b32bfcbebba44485525fe8ab69c
onyiimesi <onyedikachukwu62@gmail.com>
2025-02-08 21:52:27 +0100

    Modification

--- app/Services/User/CategoryService.php
+++ app/Services/User/CategoryService.php
@@ -199,19 +199,10 @@ class CategoryService
         $category = Category::findOrFail($id);
         $folder = App::environment('production') ? '/prod/category' : '/stag/category';
 
-        if ($request->file('image')) {
-            if (!empty($category->image)) {
-                $image = getRelativePath($category->image);
-
-                if (Storage::disk('s3')->exists($image)) {
-                    Storage::disk('s3')->delete($image);
-                }
-            }
-
+        $url = $category->image;
+        if ($request->hasFile('image')) {
             $path = $request->file('image')->store($folder, 's3');
             $url = Storage::disk('s3')->url($path);
-        } else {
-            $url = $category->image;
         }
 
         $slug = $category->slug;
