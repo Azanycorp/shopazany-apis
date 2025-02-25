9f7558e2271242277de2965ad732c5177d446a07
emmawhocoded <emmawhocoded@gmail.com>
2025-02-25 12:46:38 +0100

    Created migration for CollationCenter

---
+++ app/Models/CollationCenter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CollationCenter extends Model
+{
+    protected $fillable = [
+        'name',
+        'location',
+        'city',
+        'country',
+        'status'
+    ];
+}
