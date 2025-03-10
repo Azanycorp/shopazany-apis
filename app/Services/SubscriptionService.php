25be378618315838cdc4906a4e1880d2457521a0
onyiimesi <onyedikachukwu62@gmail.com>
2025-03-10 21:15:25 +0100

    Sub fix

--- app/Services/SubscriptionService.php
+++ app/Services/SubscriptionService.php
@@ -2,15 +2,12 @@
 
 namespace App\Services;
 
-use App\Enum\PaymentType;
-use App\Enum\UserStatus;
 use App\Http\Resources\SubscriptionHistoryResource;
 use App\Http\Resources\SubscriptionPlanResource;
 use App\Models\SubscriptionPlan;
 use App\Models\User;
 use App\Trait\HttpResponse;
 use Illuminate\Support\Facades\Auth;
-use Unicodeveloper\Paystack\Facades\Paystack;
 
 class SubscriptionService
 {
@@ -26,55 +23,6 @@ class SubscriptionService
         $data = SubscriptionPlanResource::collection($plan);
 
         return $this->success($data, "Subscription plans");
-    }
-
-    // Not in use anymore.
-    public function subscriptionPayment($request)
-    {
-        $amount = $request->input('amount') * 100;
-
-        $callbackUrl = $request->input('redirect_url');
-        if (!filter_var($callbackUrl, FILTER_VALIDATE_URL)) {
-            return $this->error(null, 'Invalid callback URL', 400);
-        }
-
-        $user = User::with([
-            'referrer' => function ($query) {
-                $query->with('wallet');
-            },
-            'userSubscriptions' => function ($query) {
-                $query->where('status', UserStatus::ACTIVE);
-            }
-        ])->findOrFail($request->user_id);
-
-        if($user->is_subscribed) {
-            $currentPlan = $user?->subscription_plan?->subscriptionPlan;
-            $newPlan = SubscriptionPlan::findOrFail($request->input('subscription_plan_id'));
-
-            if ($newPlan->tier < $currentPlan->tier) {
-                return $this->error(null, 'You cannot downgrade your subscription plan', 403);
-            }
-            if ($newPlan->id == $currentPlan->id) {
-                return $this->error(null, 'You are already subscribed to this plan', 403);
-            }
-        }
-
-        $paymentDetails = [
-            'email' => $request->input('email'),
-            'amount' => $amount,
-            'currency' => 'NGN',
-            'metadata' => json_encode([
-                'user_id' => $request->input('user_id'),
-                'referrer_id' => $user->referrer->first()?->id,
-                'subscription_plan_id' => $request->input('subscription_plan_id'),
-                'payment_method' => 'paystack',
-                'payment_type' => PaymentType::RECURRINGCHARGE,
-            ]),
-            'callback_url' => $callbackUrl
-        ];
-
-        $paystackInstance = Paystack::getAuthorizationUrl($paymentDetails);
-        return response()->json($paystackInstance);
     }
 
     public function subscriptionHistory($userId)
