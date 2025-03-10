d1ef0ae1dfca0e76244c273e07d59fbc22da4adb
onyiimesi <onyedikachukwu62@gmail.com>
2025-03-10 08:09:49 +0100

    Subcription payment modification

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enum\PaymentType;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SubscriptionPaymentRequest;
+use App\Services\Payment\AuthorizeNetSubscriptionPaymentProcessor;
+use App\Services\Payment\HandlePaymentService;
+use App\Services\Payment\PaymentDetailsService;
+use App\Services\Payment\PaystackPaymentProcessor;
 use App\Services\SubscriptionService;
+use App\Trait\HttpResponse;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
+    use HttpResponse;
+
     protected \App\Services\SubscriptionService $service;
 
     public function __construct(SubscriptionService $service)
@@ -20,9 +29,27 @@ class SubscriptionController extends Controller
         return $this->service->getPlanByCountry($countryId);
     }
 
-    public function subscriptionPayment(Request $request)
+    public function subscriptionPayment(SubscriptionPaymentRequest $request)
     {
-        return $this->service->subscriptionPayment($request);
+        $paymentProcessor = match ($request->type) {
+            PaymentType::PAYSTACK => new PaystackPaymentProcessor(),
+            PaymentType::AUTHORIZE => new AuthorizeNetSubscriptionPaymentProcessor(),
+            default => throw new \Exception("Unsupported payment method"),
+        };
+
+        $paymentService = new HandlePaymentService($paymentProcessor);
+
+        $paymentDetails = match($request->type) {
+            PaymentType::PAYSTACK => PaymentDetailsService::paystackSubcriptionPayDetails($request),
+            PaymentType::AUTHORIZE => PaymentDetailsService::authorizeNetSubcriptionPayDetails($request),
+            default => throw new \Exception("Unsupported payment method"),
+        };
+
+        if (isset($paymentDetails['error'])) {
+            return $this->error(null, $paymentDetails['error'], 400);
+        }
+
+        return $paymentService->process($paymentDetails);
     }
 
     public function subscriptionHistory($userId)
