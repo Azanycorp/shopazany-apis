cabc8858ee41ab093ae807921273886e14ac264b
emmawhocoded <emmawhocoded@gmail.com>
2025-04-10 17:07:13 +0100

    added Model for social link

--- app/Models/SocialSetting.php
+++ app/Models/SocialSetting.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialSetting extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'icon',
+        'url',
+    ];
 }
--- database/migrations/2025_04_10_141837_create_social_settings_table.php
+++ database/migrations/2025_04_10_141837_create_social_settings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('social_settings', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('icon');
+            $table->text('url');
             $table->timestamps();
         });
     }
