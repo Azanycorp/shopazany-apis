7ae86ce9cb0fbc01663abff985dcdbf9f7fc6390
emmawhocoded <emmawhocoded@gmail.com>
2025-08-05 19:08:18 +0100

    code refactoring

--- app/Services/SuperAdminService.php
+++ app/Services/SuperAdminService.php
@@ -366,6 +366,7 @@ class SuperAdminService
                 'shippment_id' => generateShipmentId(),
                 'package' => $package,
                 'customer' => $customer,
+                'item_condition' => $request->item_condition,
                 'vendor' => $vendor,
                 'status' => $request->status,
                 'priority' => $request->priority,
@@ -732,6 +733,7 @@ class SuperAdminService
 
             $shippment->update([
                 'status' => $request->status,
+                'type' => ShippmentCategory::DROPOFF,
                 'transfer_reason' => $request->transfer_reason,
                 'hub_id' => $request->hub_id,
                 'note' => $request->note,
@@ -781,7 +783,7 @@ class SuperAdminService
         return $this->success(null, 'Batch created successfully');
     }
 
-    public function processBatch($request,$id)
+    public function processBatch($request, $id)
     {
         $batch = ShippmentBatch::find($id);
 
