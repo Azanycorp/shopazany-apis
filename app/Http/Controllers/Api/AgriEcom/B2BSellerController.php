00f773a1c62f0bba3a707ee5a347419092153a52
emmawhocoded <emmawhocoded@gmail.com>
2025-08-28 21:25:10 +0100

    add methods for Shipping management

--- app/Http/Controllers/Api/AgriEcom/B2BSellerController.php
+++ app/Http/Controllers/Api/AgriEcom/B2BSellerController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api\AgriEcom;
 
+use Illuminate\Http\Request;
+use App\Services\B2B\SellerService;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ChangePasswordRequest;
-use App\Services\B2B\SellerService;
-use Illuminate\Http\Request;
+use App\Http\Requests\B2B\SellerShippingRequest;
 
 class B2BSellerController extends Controller
 {
@@ -37,5 +38,35 @@ class B2BSellerController extends Controller
     public function dashboard()
     {
         return $this->service->getDashboardDetails();
+    }
+
+    public function addShipping(SellerShippingRequest $request)
+    {
+        return $this->service->addShipping($request);
+    }
+
+    public function getAllShipping($user_id)
+    {
+        return $this->service->getAllShipping($user_id);
+    }
+
+    public function getShippingById($user_id, $shipping_id)
+    {
+        return $this->service->getShippingById($user_id, $shipping_id);
+    }
+
+    public function updateShipping(SellerShippingRequest $request, $shipping_id)
+    {
+        return $this->service->updateShipping($request, $shipping_id);
+    }
+
+    public function deleteShipping($user_id, $shipping_id)
+    {
+        return $this->service->deleteShipping($user_id, $shipping_id);
+    }
+
+    public function setDefault($user_id, $shipping_id)
+    {
+        return $this->service->setDefault($user_id, $shipping_id);
     }
 }
