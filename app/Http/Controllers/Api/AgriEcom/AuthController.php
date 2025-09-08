04da2181844e1ef8b4a77c8c4c40733529f02669
onyiimesi <onyedikachukwu62@gmail.com>
2025-09-08 16:15:53 +0100

    Login fix

--- app/Http/Controllers/Api/AgriEcom/AuthController.php
+++ app/Http/Controllers/Api/AgriEcom/AuthController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers\Api\AgriEcom;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AgriEcom\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Services\AgriEcom\AuthService;
+use App\Services\Auth\AuthService as Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function __construct(
-        protected AuthService $authService
+        protected AuthService $authService,
+        protected Auth $auth
     )
     {}
+
+    public function login(LoginRequest $request)
+    {
+        return $this->auth->login($request);
+    }
 
     public function register(RegisterRequest $request)
     {
