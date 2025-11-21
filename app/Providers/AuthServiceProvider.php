74210d317a190d1f6d3af4adb2bb6dd9f0233017
onyiimesi <onyedikachukwu62@gmail.com>
2025-11-21 11:01:22 +0100

    Code modify

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,16 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * Create a new service provider instance.
-     *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
-     */
-    public function __construct($app, private readonly \Illuminate\Contracts\Auth\Access\Gate $gate)
-    {
-        parent::__construct($app);
-    }
-
-    /**
      * Register services.
      */
     public function register(): void
@@ -29,7 +19,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->gate->before(function ($user, $ability): ?true {
+        $gate = $this->app->make(\Illuminate\Contracts\Auth\Access\Gate::class);
+        $gate->before(function ($user, $ability): ?true {
             $permissions = $user->roles->flatMap->permissions->pluck('name')->unique();
 
             return $permissions->contains($ability) ? true : null;
