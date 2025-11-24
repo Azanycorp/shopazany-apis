65139f8b26d641d458d784da4fc4212df34c62d6
onyiimesi <onyedikachukwu62@gmail.com>
2025-11-24 15:24:58 +0100

    Login sync update

--- app/Pipelines/Auth/EnsureLocalUserExists.php
+++ app/Pipelines/Auth/EnsureLocalUserExists.php
@@ -5,10 +5,13 @@ namespace App\Pipelines\Auth;
 use App\Enum\UserStatus;
 use App\Enum\UserType;
 use App\Models\User;
+use App\Trait\HttpResponse;
 use Closure;
 
 class EnsureLocalUserExists
 {
+    use HttpResponse;
+
     public function handle($request, Closure $next)
     {
         $typeValue = $request->input('type');
@@ -21,13 +24,13 @@ class EnsureLocalUserExists
         $user = User::firstOrCreate(
             ['email' => $request->email],
             [
-                'first_name' => $request->externalUser['data']['first_name'],
-                'last_name' => $request->externalUser['data']['last_name'],
-                'email' => $request->externalUser['data']['email'],
-                'country' => $request->externalUser['data']['country_id'],
-                'state_id' => $request->externalUser['data']['state_id'],
+                'first_name' => $request->externalUser['first_name'],
+                'last_name' => $request->externalUser['last_name'],
+                'email' => $request->externalUser['email'],
+                'country' => $request->externalUser['country_id'],
+                'state_id' => $request->externalUser['state_id'],
                 'type' => $type,
-                'password' => bcrypt($request->externalUser['data']['password']),
+                'password' => bcrypt($request->password),
                 'status' => UserStatus::ACTIVE,
                 'is_admin_approve' => true,
                 'is_verified' => true,
