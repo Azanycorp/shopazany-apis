20980fb52e30dbdb62beb1fad2d3015419d73d8e
onyiimesi <onyedikachukwu62@gmail.com>
2025-12-02 17:41:13 +0100

    Added cache service

--- app/Enum/ProductType.php
+++ app/Enum/ProductType.php
@@ -4,8 +4,7 @@ namespace App\Enum;
 
 enum ProductType: string
 {
+    case B2C = 'b2c';
     case AgriEcom = 'agriEcom';
-
     case B2B = 'b2b';
-
 }
--- app/Services/Cache/CacheInvalidationService.php
+++ app/Services/Cache/CacheInvalidationService.php
@@ -6,31 +6,48 @@ use Illuminate\Support\Facades\Cache;
 
 class CacheInvalidationService
 {
-    public function clearHomeServiceCache(int $countryId, string $type): void
+    public function clearHomeServiceCache(int $countryId, string $type, ?string $slug = null): void
     {
         Cache::forget("memo_best_selling_products_{$countryId}_{$type}");
         Cache::forget("memo_all_products_{$countryId}_{$type}");
         Cache::forget("memo_featured_product_{$countryId}_{$type}");
         Cache::forget("memo_top_products_{$countryId}_{$type}");
+        Cache::forget("memo_top_sellers_{$countryId}_{$type}");
+        Cache::forget("memo_recommended_products_{$countryId}_{$type}");
+        Cache::forget("memo_categories_{$type}");
+        Cache::forget("memo_category_slug_{$countryId}_{$slug}");
+        Cache::forget("memo_pocket_friendly_products_{$countryId}_{$type}");
 
         $bestSellingKey = "best_selling_products:{$countryId}:{$type}";
         $allProductsKey = "all_products:{$countryId}:{$type}";
         $featuredProductKey = "featured_product:{$countryId}:{$type}";
         $topProductsKey = "top_products:{$countryId}:{$type}";
+        $topSellersKey = "top_sellers:{$countryId}:{$type}";
+        $recommendedProductsKey = "recommended_products:{$countryId}:{$type}";
+        $categoriesKey = "categories:{$type}";
+        $categorySlugKey = "category_slug:{$countryId}:{$slug}";
+        $pocketFriendlyKey = "pocket_friendly_products:{$countryId}:{$type}";
 
-        $this->invalidateFlexibleCache($bestSellingKey, $allProductsKey, $featuredProductKey, $topProductsKey);
+        $keys = [
+            $bestSellingKey,
+            $allProductsKey,
+            $featuredProductKey,
+            $topProductsKey,
+            $topSellersKey,
+            $recommendedProductsKey,
+            $categoriesKey,
+            $categorySlugKey,
+            $pocketFriendlyKey,
+        ];
+
+        $this->invalidateFlexibleCache($keys);
     }
 
-    protected function invalidateFlexibleCache(string $bestSellingKey, string $allProductsKey, string $featuredProductKey, string $topProductsKey): void
+    protected function invalidateFlexibleCache(array $keys): void
     {
-        Cache::forget($bestSellingKey);
-        Cache::forget($allProductsKey);
-        Cache::forget($featuredProductKey);
-        Cache::forget($topProductsKey);
-
-        Cache::forget("illuminate:cache:flexible:created:{$bestSellingKey}");
-        Cache::forget("illuminate:cache:flexible:created:{$allProductsKey}");
-        Cache::forget("illuminate:cache:flexible:created:{$featuredProductKey}");
-        Cache::forget("illuminate:cache:flexible:created:{$topProductsKey}");
+        foreach ($keys as $key) {
+            Cache::forget($key);
+            Cache::forget("illuminate:cache:flexible:created:{$key}");
+        }
     }
 }
