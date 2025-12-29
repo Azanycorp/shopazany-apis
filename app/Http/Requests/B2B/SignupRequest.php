f4470c5a97383a3e1405133ebc0483f3a026866b
emmawhocoded <emmawhocoded@gmail.com>
2025-12-29 22:06:09 +0100

    make country nullable

--- app/Http/Requests/B2B/SignupRequest.php
+++ app/Http/Requests/B2B/SignupRequest.php
@@ -24,8 +24,8 @@ class SignupRequest extends FormRequest
     {
         return [
             'email' => ['required', 'email', 'email:rfc,dns', 'unique:users,email'],
-            'country_id' => ['required', 'integer', 'exists:countries,id'],
-            'state_id' => ['required', 'integer', 'exists:states,id'],
+            'country_id' => ['nullable', 'integer', 'exists:countries,id'],
+            'state_id' => ['nullable', 'integer', 'exists:states,id'],
             'password' => ['required', 'string', 'confirmed', Password::defaults()],
             'type' => ['nullable', 'string'],
         ];
