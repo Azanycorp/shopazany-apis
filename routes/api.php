d2dba861d898a02123a6fdb41d4a19ab1d98b13f
onyiimesi <onyedikachukwu62@gmail.com>
2026-03-07 08:24:36 +0100

    New API

--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,8 @@ Route::middleware('validate.header')
                 Route::get('/recommended/products', 'recommendedProducts');
                 Route::get('/single/product/{slug}', 'productSlug');
                 Route::get('/top-brands', 'topBrands');
+                Route::get('/brands', 'allBrands');
+                Route::get('/units', 'allUnits');
                 Route::get('/top-sellers', 'topSellers');
                 Route::get('/top-products', 'topProducts');
                 Route::get('/deals', 'getDeals');
