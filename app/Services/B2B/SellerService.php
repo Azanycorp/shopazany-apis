5b968297930cf1d8123d6c94d81550e334d57207
emmawhocoded <emmawhocoded@gmail.com>
2026-03-16 11:00:19 +0100

    add notification on vendor side

--- app/Services/B2B/SellerService.php
+++ app/Services/B2B/SellerService.php
@@ -30,6 +30,7 @@ use App\Models\RfqMessage;
 use App\Models\User;
 use App\Models\UserWallet;
 use App\Models\WithdrawalRequest;
+use App\Notifications\RfqMessageNotification;
 use App\Pipelines\BusinessInformation\CreateBusinessInformation;
 use App\Pipelines\BusinessInformation\UpdateUserAccount;
 use App\Repositories\B2BProductRepository;
@@ -43,6 +44,7 @@ use Illuminate\Database\DatabaseManager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Pipeline;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
@@ -744,9 +746,14 @@ class SellerService extends Controller
     public function replyRequest($request)
     {
         $rfq = Rfq::find($request->rfq_id);
+        $user = User::find($rfq->buyer_id);
+
+        if (! $user) {
+            return $this->error(null, 'Buyer not found', 404);
+        }
 
         if (! $rfq) {
-            return $this->error(null, 'No record found details', 404);
+            return $this->error(null, 'No record found for rfq', 404);
         }
 
         $amount = ($request->preferred_unit_price * $rfq->product_quantity);
@@ -762,6 +769,8 @@ class SellerService extends Controller
             'p_unit_price' => $request->preferred_unit_price,
             'total_amount' => $amount,
         ]);
+
+        Notification::send($user, new RfqMessageNotification($user, $message));
 
         return $this->success($message, 'message details');
     }
