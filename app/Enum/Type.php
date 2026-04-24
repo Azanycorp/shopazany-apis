7534a86233dc5e6adec7c19d73f182dbe1ba95a7
onyiimesi <onyedikachukwu62@gmail.com>
2026-04-24 07:51:03 +0100

    B2b authorize payment modify

---
+++ app/Enum/Type.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enum;
+
+enum Type: string
+{
+    case B2B = 'b2b';
+
+    case B2C = 'b2c';
+
+    case AGRIECOM_B2C = 'agriecom_b2c';
+
+    case AGRIECOM_B2B = 'agriecom_b2b';
+}
